7b455d7bab89ca50141808db08d8ba3d44113ebb
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-12-13 17:47:34 +0530

    (Refactor) - Removed obsolete tests

--- BowlingGameKataTests/BowlingGameKataTests.swift
+++ BowlingGameKataTests/BowlingGameKataTests.swift
@@ -14,26 +14,6 @@ class BowlingGameKataTests: XCTestCase {
         return BowlingGame()
     }()
     
-    func testCanBowlOnlyOnePoint() {
-        let score = game.bowl(knockedPins: 1)
-        XCTAssertEqual(score, 1)
-    }
-    
-    func testCanBowlOneFrame() {
-        var score = game.bowl(knockedPins: 1)
-        score = game.bowl(knockedPins: 1)
-        XCTAssertEqual(score, 2)
-    }
-    
-    func testCanBowlOneGame() {
-        var score = 0
-        for _ in 1...20 {
-            score = game.bowl(knockedPins: 1)
-        }
-        
-        XCTAssertEqual(score, 20)
-    }
-    
     func testCanBowlGutterGame() {
         var score = 0
         for _ in 1...20 {
