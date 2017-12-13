33a885f8b6d1904cf23dadf347997895005efd1e
Amin Siddiqui <amin.siddiqui_work@yahoo.com>
2017-12-13 10:17:27 +0530

    - Initial Commit

---
+++ BowlingGameKataTests/BowlingGameKataTests.swift
@@ -0,0 +1,16 @@
+//
+//  BowlingGameKataTests.swift
+//  BowlingGameKataTests
+//
+//  Created by Amin Siddiqui on 13/12/17.
+//  Copyright © 2017 svmLogics. All rights reserved.
+//
+
+import XCTest
+@testable import BowlingGameKata
+
+class BowlingGameKataTests: XCTestCase {
+    
+    
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Bowling Game Kata (TDD style)
+
+http://codingdojo.org/kata/Bowling/
